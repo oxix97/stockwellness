9e6f7e804320ccb7439c248e652e3493878fc135
Chan <ieejo716@naver.com>
2025-12-08 15:11:52 +0900

    [Test] 테스트 환경 설정

---
+++ src/test/java/org/stockwellness/config/TestJpaConfig.java
@@ -0,0 +1,9 @@
+package org.stockwellness.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@TestConfiguration
+public class TestJpaConfig {
+}
