dfd68640f2edd99747de404188ecb3d495fa79f8
Chan <ieejo716@naver.com>
2025-12-23 18:47:11 +0900

    [Setting] RestClient 설정

---
+++ src/main/java/org/stockwellness/config/RestClientConfig.java
@@ -0,0 +1,20 @@
+package org.stockwellness.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${krx.base-url}")
+    private String baseUrl;
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+}
