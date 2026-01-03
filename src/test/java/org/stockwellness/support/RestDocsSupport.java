e09218cbb735d0b96ae548c879d3969a27359cbe
Chan <72330632+oxix97@users.noreply.github.com>
2026-01-03 17:16:41 +0900

    Setting sub/#28 swagger restdocs (#31) (#32)
    * [Chore] Swagger, REST doc 의존성 추가
    
    * [Chore] 테스트 환경 변수
    
    * [Chore] swagger-ui 자동 생성 결과물
    
    * [Feature] Swagger-ui 설정
    
    * [Test] swagger 테스트 항목 작성

---
+++ src/test/java/org/stockwellness/support/RestDocsSupport.java
@@ -0,0 +1,43 @@
+package org.stockwellness.support;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ActiveProfiles("test")
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class RestDocsSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(MockMvcRestDocumentation.documentationConfiguration(restDocumentation)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())  // 요청 JSON 예쁘게 출력
+                        .withResponseDefaults(prettyPrint()) // 응답 JSON 예쁘게 출력
+                )
+                .build();
+    }
+}
