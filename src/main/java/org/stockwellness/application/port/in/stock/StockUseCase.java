7831f7c27967b1ea6eb0221f2ded3189d41b85ec
Chan <ieejo716@naver.com>
2026-02-14 16:27:59 +0900

    eat(stock): 종목 탐색 API 구현 및 REST Docs/OpenAPI 문서화

--- src/main/java/org/stockwellness/application/port/in/stock/StockUseCase.java
+++ src/main/java/org/stockwellness/application/port/in/stock/StockUseCase.java
@@ -5,7 +5,10 @@ import org.stockwellness.application.port.in.stock.query.SearchStockQuery;
 import org.stockwellness.application.port.in.stock.result.StockDetailResult;
 import org.stockwellness.application.port.in.stock.result.StockSearchResult;
 
-public interface StockReadUseCase {
+import java.util.List;
+
+public interface StockUseCase {
     Slice<StockSearchResult> searchStocks(SearchStockQuery query);
     StockDetailResult getStockDetail(String ticker);
+    List<StockSearchResult> getNewListings();
 }
