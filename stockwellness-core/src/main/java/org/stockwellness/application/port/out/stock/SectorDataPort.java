fa0adc181dcab5b79ec5426c0e808df110a94ff4
Chan <ieejo716@naver.com>
2026-02-26 18:33:27 +0900

    refactor: 아웃바운드 포트 통합 및 KIS 멀티 종목 시세 API 연동

---
+++ stockwellness-core/src/main/java/org/stockwellness/application/port/out/stock/SectorDataPort.java
@@ -0,0 +1,29 @@
+package org.stockwellness.application.port.out.stock;
+
+import org.stockwellness.adapter.out.external.kis.dto.InvestorTradingDaily;
+import org.stockwellness.adapter.out.external.kis.dto.KisDailySectorDetail;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface SectorDataPort {
+    /**
+     * 특정 업종 코드의 당일 상세 지수 정보를 가져옵니다.
+     */
+    KisDailySectorDetail fetchDailySectorDetail(String indexCode);
+
+    /**
+     * 특정 업종 코드의 최근 N일간 투자자 매매동향을 가져옵니다.
+     */
+    List<InvestorTradingDaily> fetchInvestorTradingDaily(String indexCode, int days);
+
+    /**
+     * 특정 업종 코드의 과거 지수 이력을 가져옵니다. (MA, RSI 계산용)
+     */
+    List<BigDecimal> fetchHistoricalIndexPrices(String indexCode, int days);
+
+    /**
+     * 당일 전체 업종 데이터를 가져옵니다. (배치 Reader 용)
+     */
+    List<SectorApiDto> fetchTodaySectorData();
+}
