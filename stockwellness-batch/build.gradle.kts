828d659718bdb60cb12cde4b75faa2ca8b412c25
Chan <ieejo716@naver.com>
2026-03-09 19:59:49 +0900

    chore: spring actuator 설정

--- stockwellness-batch/build.gradle.kts
+++ stockwellness-batch/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     // kafka
     implementation("org.springframework.kafka:spring-kafka")
